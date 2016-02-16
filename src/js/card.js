5cfbaffa7af8435d6f58012d376ba7650efd0dcc
nltesown <nlte@magic.fr>
2016-02-16 16:23:43 +0100

    Extend points collection with reference to cards

--- src/js/card.js
+++ src/js/card.js
@@ -1,12 +1,25 @@
-/* Card */
+/**
+ * Card
+ * Manages the card pane (right half of window)
+ *
+ */
 
 var card = (function () {
   var elemCardContainer = document.querySelector(".cardContainer");
 
+
   function show(point) {
-    $(elemCardContainer).append("<span>" + point.name + "<span>");
+
+
+    _.forEach(point.cards, function (c) {
+      var cardData = _.find(cards, { id: c });
+      $("<div class='card'><h2>" + cardData.id + "</h2></div>").appendTo(elemCardContainer);
+    });
+
   }
+
   return {
     show: show
   };
+
 })();
--- src/js/main.js
+++ src/js/main.js
@@ -5,18 +5,36 @@
 map.load("data/world-110m.json", "data/places.json");
 
 map.on("ready", function () {
-  var points = map.points();
 
-  map.on("click", function (e, point) {
-    $(_.map(points, "svg")).removeClass("on");
-    $(point.svg).addClass("on");
-    map.panTo(point);
-    card.show(point);
+
+  d3_queue.queue()
+  .defer(d3.json, "data/cards.json")
+  .awaitAll(function (error, data) {
+    if (error) throw error;
+
+    var cards = data[0];
+
+    var points = _.map(map.points(), function (p) { // map.points() is extended with a cards property that holds the array of ids of cards referencing this point
+      return _.assign(p, { cards: _.map(_.filter(cards, function (c) {
+        return _.indexOf(c.points, p.id) > -1;
+      }), "id") });
+    });
+
+    // DOM Bindings
+    map.on("click", function (e, point) {
+      $(_.map(points, "svg")).removeClass("on");
+      $(point.svg).addClass("on");
+      map.panTo(point);
+      card.show(point);
+    });
+
+    // Just for fun: night and day toggler
+    $(".ctrl-day").on("click", function () {
+      $("svg").toggleClass("night");
+    });
+
+
   });
-
-  // Just for fun: night and day toggler
-  $(".ctrl-day").on("click", function () {
-    $("svg").toggleClass("night");
-  });
-
 });
+
+
