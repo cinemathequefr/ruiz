0f5301b9492a392628a94b2278eea6cc3de06615
nltesown <nlte@magic.fr>
2016-03-15 18:01:20 +0100

    Update card template

--- src/js/card.js
+++ src/js/card.js
@@ -10,8 +10,13 @@ var card = (function () {
 
   var renderTemplate = {};
   _.templateSettings.interpolate = /{{([\s\S]+?)}}/g; // Set mustache-style interpolate delimiters
-  renderTemplate.card = _.template("<div class='card' data-id='{{ id }}'><div class='mediaContainer'><% _.forEach(assets, function (asset) { %><div class='asset'><% if (asset.type === 'img') { %><img src='http://cf.pasoliniroma.com/static/ruiz/img/{{ asset.id }}.jpg'><% } else if (asset.type === 'video') { %><iframe class='video' src='//player.vimeo.com/video/{{ asset.id }}' frameborder='0'></iframe><% } %></div><% }); %></div><p><span class='title'>{{ title }}.</span> {{ text }}</p><ul class='places'><% _.forEach(points, function (p) { %><li data-pointId='{{ p.id }}'>{{ p.name }}</li><% }); %></ul></div>");
   // renderTemplate.card = _.template("<div class='card' data-id='{{ id }}'><div class='mediaContainer'><% _.forEach(assets, function (asset) { %><div class='asset'><% if (asset.type === 'img') { %><img src='http://cf.pasoliniroma.com/static/ruiz/img/{{ asset.id }}.jpg'><% } else if (asset.type === 'video') { %><iframe class='video' src='//player.vimeo.com/video/{{ asset.id }}' frameborder='0'></iframe><% } %></div><% }); %></div><p><span class='title'>{{ title }}.</span> {{ text }}</p><ul class='places'><% _.forEach(points, function (p) { %><li data-pointId='{{ p.id }}'>{{ p.name }}</li><% }); %></ul><ul class='films'><% _.forEach(films, function (f) { %><a href='http://www.cinematheque.fr/film/{{ f.id }}.html'>{{ f.title }}</a></li><% }); %></ul></div>");
+
+
+  // renderTemplate.card = _.template("<div class='card' data-id='{{ id }}'><div class='mediaContainer'><% _.forEach(assets, function (asset) { %><div class='asset'><% if (asset.type === 'img') { %><img src='http://cf.pasoliniroma.com/static/ruiz/img/{{ asset.id }}.jpg'><% } else if (asset.type === 'video') { %><iframe class='video' src='//player.vimeo.com/video/{{ asset.id }}' frameborder='0'></iframe><% } %></div><% }); %></div><p><span class='title'>{{ title }}.</span> {{ text }}</p><ul class='places'><% _.forEach(points, function (p) { %><li data-pointId='{{ p.id }}'>{{ p.name }}</li><% }); %></ul></div>");
+  renderTemplate.card = _.template("<div class='card' data-id='{{ id }}'><div class='mediaContainer'><% _.forEach(assets, function (asset) { %><div class='asset'><% if (asset.type === 'img') { %><img src='http://cf.pasoliniroma.com/static/ruiz/img/{{ asset.id }}.jpg'><% } else if (asset.type === 'video') { %><iframe class='video' src='//player.vimeo.com/video/{{ asset.id }}' frameborder='0'></iframe><% } %></div><% }); %></div><p><span class='title'>{{ currentPoint.name }} : {{ title }}.</span> {{ text }}</p><ul class='places'><% _.forEach(_.filter(points, function (p) { return p.id !== currentPoint.id; }), function (p) { %><li data-pointId='{{ p.id }}'>{{ p.name }}</li><% }); %></ul></div>");
+
+
 
   var points, cards;
   var deck = []; // Array: DOM elements of cards
@@ -24,11 +29,11 @@ var card = (function () {
   function show(point) {
 
     _.forEach(_.reverse(point.cards), function (card) {
-
       // Development only (lorem ipsum)
       card.text = card.text || "Quam quidem partem accusationis admiratus sum et moleste tuli potissimum esse Atratino datam. Neque enim decebat neque aetas illa postulabat neque, id quod animadvertere poteratis, pudor patiebatur optimi adulescentis in tali illum oratione versari. Vellem aliquis ex vobis robustioribus hunc male dicendi locum suscepisset; aliquanto liberius et fortius et magis more nostro refutaremus istam male dicendi licentiam. Tecum, Atratine, agam lenius, quod et pudor tuus moderatur orationi meae et meum erga te parentemque tuum beneficium tueri debeo.";
 
-      deck.push($(renderTemplate.card(card)));
+      // deck.push($(renderTemplate.card(_.assign({}, card, { currentPoint: point.id })))); // Extend card with currentPoint id (used by the card template)
+      deck.push($(renderTemplate.card(_.assign({}, card, { currentPoint: { id: point.id, name: point.name } })))); // Extend card with currentPoint id/name (used by the card template)
     });
 
     (function display(deck) {
